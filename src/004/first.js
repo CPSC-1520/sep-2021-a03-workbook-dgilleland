2ada3cf956ff3a515f9ce878083e45260e156086
Dan Gilleland <dgilleland@nait.ca>
2021-09-18 09:21:01 -0600

    Lesson 4 demo complete

--- src/004/first.js
+++ src/004/first.js
@@ -1,1 +1,3 @@
+// The alert function pops up a "modal dialog" that the user must
+// interact with before the script can continue running.
 alert('I am about to load the body of this page.');
--- src/004/second.js
+++ src/004/second.js
@@ -1,4 +1,7 @@
+// The browser's console is a place where we can also put messages.
+// I am "logging" a message to the console.
 console.log('Waiting a little bit....');
+// This is a "delay" of 3 seconds (3000 milliseconds)
 await new Promise(resolve => setTimeout(resolve, 3000));
 console.log('I am done waiting.');
 alert('The rest of the page should have loaded and rendered by the time this alert popped up.');
