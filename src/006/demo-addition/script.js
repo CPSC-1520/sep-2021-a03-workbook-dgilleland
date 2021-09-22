5f5fa0430cecc97d715bdb4ff766d0b423ae6d62
Dan Gilleland <dgilleland@nait.ca>
2021-09-22 14:24:51 -0600

    Improve the Demo Addition of Lesson 6

--- src/006/demo-addition/script.js
+++ src/006/demo-addition/script.js
@@ -1,20 +1,49 @@
 // Add in the code from the walk-through and observe the page behaviour.
-let first = prompt('Enter a number.');
-let second = prompt('Enter another number.');
 
-// ensure that I have a numeric data type
-first = parseFloat(first);
-second = parseFloat(second);
+const addNumbers = function (firstParam, secondParam) {
+    let result = 0;
+    result += parseFloat(firstParam);
+    result += parseFloat(secondParam);
+    return result;
+}
+// Create three other functions: multiplyNumbers, subtractNumbers, divideNumbers
+const multiplyNumbers = function (firstParam, secondParam) {
+    return parseFloat(firstParam) * parseFloat(secondParam);
+}
 
-let result = first + second;
+const subtractNumbers = function (firstParam, secondParam) {
+    return parseFloat(firstParam) - parseFloat(secondParam);
+}
 
-alert(`I calculated a result of ${result} when adding the numbers.`);
+const divideNumbers = function (firstParam, secondParam) {
+    return parseFloat(firstParam) / parseFloat(secondParam);
+}
 
-firstNumber.innerText = first;
-secondNumber.innerText = second;
-answer.innerText = result;
+const setUp = function() {
+    let first = prompt('Enter a number.');
+    let second = prompt('Enter another number.');
+    firstNumber.innerText = first;
+    secondNumber.innerText = second;
+}
 
-let repeat = confirm('Again?');
+const showResults = function(title, symbol, calculation) {
+    // perform the calculation
+    let result = calculation(firstNumber.innerText, secondNumber.innerText);
+    
+    // set the outputs
+    operation.innerText = title;
+    operator.innerText = symbol;
+    answer.innerText = result;
+}
 
-let para = document.querySelector('p'); // grab the first <p>
-para.innerHTML = repeat ? 'Press <a href="#">this link</a> to try again.' : 'Thanks for playing';
+
+// let result = addNumbers(first, second); // first + second;
+
+// alert(`I calculated a result of ${result} when adding the numbers.`);
+
+// answer.innerText = result;
+
+// let repeat = confirm('Again?');
+
+// let para = document.querySelector('p'); // grab the first <p>
+// para.innerHTML = repeat ? 'Press <a href="#">this link</a> to try again.' : 'Thanks for playing';
