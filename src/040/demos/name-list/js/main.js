a4b123e5bc26c9cffdc63fb342fb9b1a5d5639e8
Dan Gilleland <dgilleland@nait.ca>
2021-10-18 11:45:22 -0600

    Finished intro to looping

--- src/040/demos/name-list/js/main.js
+++ src/040/demos/name-list/js/main.js
@@ -1,20 +1,26 @@
 /*
-	TASK: prompt for individual names and add them to 
-		  a list.  Confirm after each name is entered
-		  whether the user would like to enter another
-		  name or not.
+	TASK: prompt for individual names and add them to a list.  Confirm after each name is entered whether the user would like to enter another name or not.
 */
-let outDiv = document.querySelector('div');
 
-let myHTML = '<ul>';
+// Avoid Global Variables and Global Execution
 
-// Add my names
-do {
-    let aName = prompt('Enter name: ');
-    myHTML += '<li>' + aName + '</li>';
-} while(confirm('Another name?'));
+const readyToRun = function() {
+	let outDiv = document.querySelector('div');
 
-myHTML += '</ul>';
+	let myHTML = '';
 
-// display the list
-outDiv.innerHTML = myHTML;
+	// Add my names
+	do {
+		let aName = prompt('Enter name: ');
+		myHTML += `<li>${aName}</li>`; // template literal
+	} while(confirm('Another name?'));
+
+	myHTML = `<ul>${myHTML}</ul>`; // template literal
+
+	// display the list
+	outDiv.innerHTML = myHTML;
+}
+
+// There is one global execution that I will allow:
+// The "main" function to run when the document has loaded
+document.addEventListener('DOMContentLoaded', readyToRun);
